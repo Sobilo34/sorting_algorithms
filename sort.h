3589cb75bee78a8b536d0767740100ce5a32872d
Gideon Oba <deyonoba@gmail.com>
2024-01-20 20:52:28 +0000

    update: sort.h with bubble sort function prototype

--- sort.h
+++ sort.h
@@ -4,6 +4,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 /**
  * struct listint_s - Doubly linked list node
@@ -22,4 +23,5 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 
+void bubble_sort(int *array, size_t size);
 #endif
