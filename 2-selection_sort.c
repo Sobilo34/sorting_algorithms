b81f61bbec8ed380ada95609ce3b69e07c05d3f4
Gideon Oba <deyonoba@gmail.com>
2024-01-22 06:15:12 +0000

    Add 2-selection_sort.c file

---
+++ 2-selection_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * selection_sort - Implement selection sort algorithm
+ *
+ * @array: An array of integers
+ * @size: Number of elements in array
+ */
+void selection_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int min, temp;
+	bool did_min_change;
+
+	if (!array || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		min = array[i];
+		did_min_change = false;
+		for (j = i; j < size - 1; j++)
+		{
+			if (min > array[j + 1])
+			{
+				temp = array[j + 1];
+				array[j + 1] = min;
+				min = temp;
+				did_min_change = true;
+			}
+		}
+		if (!did_min_change)
+			break;
+
+		array[i] = min;
+		print_array(array, size);
+	}
+}
