d463ec5025682adbb40a869872e509dd760c9d73
Sobilo34 <bilalsolih60@gmail.com>
2024-01-24 01:59:04 -0800

    sort.h now betty compliant

--- sort.h
+++ sort.h
@@ -29,16 +29,19 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1
 , listint_t *node2);
-int partition_with_lomuto(int array[], int lowerband, int upperband, size_t arr_size);
+int partition_with_lomuto(int array[], int lowerband,
+		int upperband, size_t arr_size);
 void swap(int *element1, int *element2);
 void quick_sort(int *array, size_t size);
-void main_sort(int *array,int lowerband, int upperband, size_t size);
+void main_sort(int *array, int lowerband,
+		int upperband, size_t size);
 void shell_sort(int *array, size_t size);
 void counting_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 void main_sort_hoare(int *array, int lowerband, int upperband, size_t size);
 void swap_print(int *array, int i, int j, size_t size);
-int partition_with_hoare(int *array, int upperband, int lowerband, size_t arr_size);
+int partition_with_hoare(int *array, int upperband,
+		int lowerband, size_t arr_size);
 
 
 #endif
